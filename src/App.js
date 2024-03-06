ec0c0d0eecd396d48f04dec9bb18cb01dc642788
Arman Z. Papyan <Arman.Z.Papyan@ad.ucom.am>
2024-03-06 16:35:55 +0400

    end task

--- src/App.js
+++ src/App.js
@@ -7,30 +7,40 @@ import {useEffect, useState} from "react";
 
 
 function App() {
-     const [query,setQuery] = useState(0)
-    const [changeValue,setChangeValue] = useState('')
-    const [from, setFrom]=useState('EUR')
-    const [to, setTo]=useState('USD')
+    const [query, setQuery] = useState(100)
+    const [changeValue, setChangeValue] = useState('')
+    const [from, setFrom] = useState('USD')
+    const [to, setTo] = useState('EUR')
+    const arr = ['USD', 'EUR', 'CAD', 'INR']
+    const [isLoading, setIsLoading] = useState(false)
+    const HandleEditQuery = (e) => {
+        setQuery(e.target.value)
+    }
+    const handleChangeFrom = {}
 
-
-    useEffect(()=>{
-        async function fetchmaneyCalculiate (){
+    useEffect(() => {
+        async function fetchmaneyCalculiate() {
             try {
-                const res =  await   fetch(`https://api.frankfurter.app/latest?amount=${query}&from=${from}&to=${to}`)
-                if (!res.ok){
+                if (query <= 0) return
+                setIsLoading(true)
+                const res = await fetch(`https://api.frankfurter.app/latest?amount=${query}&from=${from}&to=${to}`)
+                if (!res.ok) {
                     throw new Error('sxal harcum')
                 }
                 const data = await res.json()
                 if (data.Response === 'False') {
                     throw new Error('Value not found')
                 }
-                setChangeValue(data)
-            }catch (err){
+                setChangeValue(data.rates[to])
+            } catch (err) {
                 console.error(err)
             }
         }
+
         fetchmaneyCalculiate()
-    },[])
+        setIsLoading(false)
+
+    }, [query, from, to, changeValue])
 
     console.log(changeValue)
     console.log(query)
@@ -38,24 +48,37 @@ function App() {
     console.log(to)
 
 
-  return (
-    <div className="App">
-       <input type="text" value={query} onChange={e=>{setQuery(e.target.value)}}/>
-      <select value={from} onChange={(e)=>{setFrom(e.target.value)}}>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select value={to} onChange={e=>setTo(e.target.value)}>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <input disabled={isLoading} type="number" value={query} onChange={e => HandleEditQuery(e)}/>
+            <select
+                value={from}
+                onChange={(e) => {
+                    setFrom(e.target.value)
+                }}
+                disabled={isLoading}
+            >
+
+                {
+                    arr.filter(item => item !== to).map((item) => {
+                        return <option value={item}>{item}</option>
+                    })
+                }
+            </select>
+            <select
+                value={to}
+                onChange={e => setTo(e.target.value)}
+                disabled={isLoading}
+            >
+                {
+                    arr.filter(item => item !== from).map((item) => {
+                        return <option value={item}>{item}</option>
+                    })
+                }
+            </select>
+            {isLoading ? <p>Loadding...</p> : <p>{changeValue} {to}</p>}
+        </div>
+    );
 }
 
 export default App;
